71f6a0012b8cf893c5e72a6f3bf42bc9ba86dae5
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-08 17:09:26 -0700

    apt + libcurl related stuff

--- site-cookbooks/opsworks-commons/recipes/default.rb
+++ site-cookbooks/opsworks-commons/recipes/default.rb
@@ -10,3 +10,24 @@
 # include_recipe 'apt::default'
 include_recipe 'build-essential::default'
 include_recipe 'java'
+
+execute "apt-get-update" do
+  command "apt-get update"
+  ignore_failure true
+  only_if do
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+  end
+end
+
+packages = [
+  "libcurl3",
+  "libcurl3-gnutls",
+  "libcurl4-openssl-dev"
+]
+
+packages.each do |p|
+  package p do
+    action :install
+  end
+end
