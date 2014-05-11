fa82f4d19d2c8480e41f2e936be07b0ffdd10e67
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-11 07:42:18 -0700

    mongodb repl test 11

--- site-cookbooks/opsworks-commons/attributes/default.rb
+++ site-cookbooks/opsworks-commons/attributes/default.rb
@@ -1,11 +1,8 @@
 node.override['mongodb'] = {
-    "replicaset_name" => "CB Rpl 1",
     "cluster_name" => "CB Rpl 1",
      "config" => {
-       "rest" => "true",
+       "rest" => "false",
        "bind_ip" => "0.0.0.0",
-       "replicaset_name" => "CB Rpl 1",
-       "cluster_name" => "CB Rpl 1",
        "replSet" => "CB Rpl 1"
     }
 }
--- site-cookbooks/opsworks-commons/attributes/mongods-rpl.rb
+++
@@ -1,8 +0,0 @@
-node.override['mongodb'] = {
-    "replicaset_name" => "CB Rpl 1",
-    "cluster_name" => "CB Rpl 1",
-     "config" => {
-       "rest" => "true",
-       "bind_ip" => "0.0.0.0"
-    }
-}
