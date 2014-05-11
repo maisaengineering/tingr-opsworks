5017ca139a0087c618561e71189c88e3952ebb44
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-11 08:02:08 -0700

    image magic added part of new recipe, webutils

---
+++ site-cookbooks/opsworks-commons/recipes/webutils.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: opsworks-commons
+# Recipe:: webutils
+#
+# Copyright (C) 2014 Chandan Benjaram
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "imagemagick"
