33f9c6d88100303f5ed34dc03ddbf4d92ba87b9a
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-11 07:49:40 -0700

    mongodb repl test 12

--- site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
+++ site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
@@ -9,5 +9,21 @@
 
 # node.set['mongodb']['cluster_name'] = 'CB Test cluster'
 
+node.set['mongodb']['cluster_name'] = "CB Rpl 786"
+node.set['mongodb']['config']['rest'] = "true"
+node.set['mongodb']['config']['bind_ip'] = "0.0.0.0"
+node.set['mongodb']['config']['replSet'] = "CB Rpl 786"
+
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::replicaset"
+
+
+
+# node.set['mongodb'] = {
+#     "cluster_name" => "CB Rpl 1",
+#      "config" => {
+#        "rest" => "false",
+#        "bind_ip" => "0.0.0.0",
+#        "replSet" => "CB Rpl 1"
+#     }
+# }
