6704670fab3b1e853bcf4e7533d01999785cd83c
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-11 05:39:50 -0700

    mongodb repl test

---
+++ site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: opsworks-commons
+# Recipe:: mongods-rpl
+#
+# Copyright (C) 2014 Chandan Benjaram
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node.set['mongodb']['cluster_name'] = 'CB Test cluster'
+
+include_recipe "mongodb::10gen_repo"
+include_recipe "mongodb::replicaset"
+
+
+mongodb_instance "mongodb" do
+  dbpath node['application']['cbdb_path']
+  replSet   "CBRepl Set"
+end
