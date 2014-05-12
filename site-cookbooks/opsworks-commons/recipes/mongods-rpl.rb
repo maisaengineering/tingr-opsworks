4416f662f9f8d918642f37abfd691b54cf0f08b3
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-12 12:22:00 -0700

    berks update

--- site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
+++ site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
@@ -9,21 +9,19 @@
 
 # node.set['mongodb']['cluster_name'] = 'CB Test cluster'
 
-node.set['mongodb']['cluster_name'] = "CB Rpl 786"
-node.set['mongodb']['config']['rest'] = "true"
-node.set['mongodb']['config']['bind_ip'] = "0.0.0.0"
-node.set['mongodb']['config']['replSet'] = "CB Rpl 786"
-
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::replicaset"
 
-
-
 # node.set['mongodb'] = {
-#     "cluster_name" => "CB Rpl 1",
+#     "cluster_name" => "KLReplicaSet",
 #      "config" => {
-#        "rest" => "false",
+#        "rest" => "true",
 #        "bind_ip" => "0.0.0.0",
-#        "replSet" => "CB Rpl 1"
+#        "replSet" => "KLReplicaSet"
 #     }
 # }
+
+node.set['mongodb']['cluster_name'] = "KLReplicaSet"
+node.set['mongodb']['config']['rest'] = "true"
+node.set['mongodb']['config']['bind_ip'] = "0.0.0.0"
+node.set['mongodb']['config']['replSet'] = "KLReplicaSet"
