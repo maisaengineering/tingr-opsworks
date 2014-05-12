9fe9d17596a0b17fa9bcbfdf858de6bb6025cf06
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-12 12:42:44 -0700

    berks update

--- site-cookbooks/opsworks-commons/attributes/default.rb
+++ site-cookbooks/opsworks-commons/attributes/default.rb
@@ -1,8 +1,8 @@
-node.override['mongodb'] = {
-    "cluster_name" => "CB Rpl 1",
-     "config" => {
-       "rest" => "false",
-       "bind_ip" => "0.0.0.0",
-       "replSet" => "CB Rpl 1"
-    }
-}
+# node.override['mongodb'] = {
+#     "cluster_name" => "CB Rpl 1",
+#      "config" => {
+#        "rest" => "false",
+#        "bind_ip" => "0.0.0.0",
+#        "replSet" => "CB Rpl 1"
+#     }
+# }
