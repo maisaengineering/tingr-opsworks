ded7eba1045e07174d665027f9f4234730f06d2f
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-12 16:41:08 -0700

    berks update

--- site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
+++ site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
@@ -9,7 +9,7 @@
 
 # node.set['mongodb']['cluster_name'] = 'CB Test cluster'
 
-Chef::Resource::User.send(:include, Chef::ResourceDefinitionList::OpsWorksHelper)
+# Chef::Resource::User.send(:include, Chef::ResourceDefinitionList::OpsWorksHelper)
 
 node.override['mongodb'] = {
     "cluster_name" => "KLReplicaSet",
@@ -20,26 +20,31 @@ node.override['mongodb'] = {
     }
 }
 
+Chef::Log.info('MongoDB solo installing...')
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::default"
+Chef::Log.info('...done')
 
-Chef::Log.info('MongoDB solo install went through')
-
-Chef::Log.info('Including helpers from MongoDB Cookbook...')
+# Chef::Log.info('Including helpers from MongoDB Cookbook...')
 # ::Chef::Recipe.send(:include, Chef::ResourceDefinitionList::MongoDB)
 # Chef::Resource::User.send(:include, Chef::ResourceDefinitionList::MongoDB)
 
-Chef::Log.info('...helper included successfully')
+# Chef::Log.info('...helper included successfully')
 
 # assuming for the moment only one layer for the replicaset instances
+
+Chef::Log.info('reading replicaset_layer_slug_name...')
+is_ops_works = Chef::ResourceDefinitionList::OpsWorksHelper.opsworks?
+Chef::Log.info("is_ops_works => #{is_ops_works}")
+
 Chef::Log.info('reading replicaset_layer_slug_name...')
 replicaset_layer_slug_name = node['opsworks']['instance']['layers'].first
-Chef::Log.info('replicaset_layer_slug_name = #{replicaset_layer_slug_name}')
+Chef::Log.info("replicaset_layer_slug_name = #{replicaset_layer_slug_name}")
 
 Chef::Log.info('reading replicaset_layer_instances...')
 replicaset_layer_instances = node['opsworks']['layers'][replicaset_layer_slug_name]['instances']
-Chef::Log.info('replicaset_layer_instances = #{replicaset_layer_instances}')
-
+Chef::Log.info("replicaset_layer_instances = #{replicaset_layer_instances}")
+Chef::ResourceDefinitionList::OpsWorksHelper.configure_replicaset(node, replicaset_layer_slug_name, replicaset_members(node, replicaset_layer_instances))
 
 # OpsWorksHelper.configure_replicaset(new_resource.replicaset, replicaset_name, replicaset_layer_instances)
 
@@ -59,4 +64,5 @@ Chef::Log.info('replicaset_layer_instances = #{replicaset_layer_instances}')
 
 
 #MongoDB.configure_replicaset(new_resource.replicaset, replicaset_name, replicaset_layer_instances)
-MongoDB.configure_replicaset(new_resource.replicaset, replicaset_name, rs_nodes) unless new_resource.replicaset.nil?
+Chef::Log.info('calling...mongodb helper')
+Chef::ResourceDefinitionList::MongoDB.configure_replicaset(new_resource.replicaset, replicaset_name, rs_nodes) unless new_resource.replicaset.nil?
