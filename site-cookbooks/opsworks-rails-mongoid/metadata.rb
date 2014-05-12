159f56b7a018f0dc707fa4a7ec9b9729e1949fce
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-12 08:11:35 -0700

    stripped version of fork netinlet/chef-mongodb_replicaset

---
+++ site-cookbooks/opsworks-rails-mongoid/metadata.rb
@@ -0,0 +1,9 @@
+name             'opsworks-rails-mongoid'
+maintainer       'Chandan Benjaram'
+maintainer_email 'labs@maisasolutions.com'
+license          'All rights reserved'
+description      'Installs/Configures opsworks-rails-mongoid'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'deploy'
---
+++ site-cookbooks/opsworks-rails-mongoid/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: opsworks-rails-mongoid
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
