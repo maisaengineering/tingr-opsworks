feceb33853666cd858a31b15e5575f2bd47ba660
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-13 05:16:20 -0700

    mongodb repl update

--- site-cookbooks/opsworks-rails-mongoid/recipes/configure.rb
+++ site-cookbooks/opsworks-rails-mongoid/recipes/configure.rb
@@ -12,11 +12,6 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  execute "restart Rails app #{application}" do
-    cwd deploy[:current_path]
-    command node[:opsworks][:rails_stack][:restart_command]
-    action :nothing
-  end
 
   node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
   deploy = node[:deploy][application]
@@ -34,7 +29,7 @@ node[:deploy].each do |application, deploy|
       :replicaset_instances => replicaset_instances
     )
 
-    notifies :run, "execute[restart Rails app #{application}]"
+    notifies :run, "execute[restart Rails app #{application}]", :immediately
 
     only_if do
       File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
@@ -54,7 +49,17 @@ node[:deploy].each do |application, deploy|
       :replicaset_instances => replicaset_instances
     )
 
-    notifies :run, "execute[restart Rails app #{application}]"
+    notifies :run, "execute[restart Rails app #{application}]", :immediately
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
+
+  execute "Restart Rails stack #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
   end
 
 end
