820691447bf2c375c94c4f8b14a497750dae789e
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-13 00:31:23 -0700

    mongodb repl update

--- site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
+++ site-cookbooks/opsworks-commons/recipes/mongods-rpl.rb
@@ -27,6 +27,10 @@ node.override['mongodb'] = {
     "ruby_gems" => { :mongo => nil,:bson_ext => nil }
 }
 
+default['mongodb']['config']['replSet'] = "KLReplicaSet"
+node.override['mongodb']['replicaset_name'] = "KLReplicaSet"
+
+
 Chef::Log.info('MongoDB solo installing...')
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::default"
