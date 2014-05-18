f3f9fc0d3f65e1441da390192b67138af2a6700e
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-18 00:34:20 -0700

    mognodb conf setup

--- site-cookbooks/opsworks-rails-mongoid/libraries/opsworks_ormhelper.rb
+++ site-cookbooks/opsworks-rails-mongoid/libraries/opsworks_ormhelper.rb
@@ -8,46 +8,46 @@ class Chef::ResourceDefinitionList::OpsWorksORMHelper
   end
 
   # return Chef Nodes for this replicaset / layer
-  def self.replicaset_members_with_mongo_conf(node)
-    members = []
-    # FIXME -> this is bad, we're assuming replicaset instances use a single layer
-    node_layer = node['opsworks']['instance']['layers'].first
-    instances = node['opsworks']['layers'][replicaset_layer_slug_name]['instances']
-    instances.each do |name, instance|
-      if instance['status'] == 'online'
-        member = Chef::Node.new
-        new_name = "#{name}.localdomain"
-        member.name(name)
-        member.default['fqdn'] = instance['private_dns_name']
-        member.default['ipaddress'] = instance['private_ip']
-        member.default['hostname'] = new_name
-        mongodb_attributes = {
-          # here we could support a map of instances to custom replicaset options in the custom json
-          'port' => node['mongodb']['config']['port'],
-          'replica_arbiter_only' => false,
-          # 'replica_build_indexes' => false,
-          'replica_hidden' => false,
-          'replica_slave_delay' => 0,
-          'replica_priority' => instance['private_ip'].gsub(/^.*\.(?=\d+)/, '').to_i,
-          'replica_tags' => {}, # to_hash is called on this
-          'replica_votes' => 1,
-          'config' => {
-            'port' => node['mongodb']['config']['port'],
-            'replica_arbiter_only' => false,
-            # 'replica_build_indexes' => true,
-            'replica_hidden' => false,
-            'replica_slave_delay' => 0,
-            'replica_priority' => instance['private_ip'].gsub(/^.*\.(?=\d+)/, '').to_i,
-            'replica_tags' => {}, # to_hash is called on this
-            'replica_votes' => 1
-          }
-        }
-        member.default['mongodb'] = mongodb_attributes
-        members << member
-      end
-    end
-    members
-  end
+  # def self.replicaset_members_with_mongo_conf(node)
+  #   members = []
+  #   # FIXME -> this is bad, we're assuming replicaset instances use a single layer
+  #   node_layer = node['opsworks']['instance']['layers'].first
+  #   instances = node['opsworks']['layers'][replicaset_layer_slug_name]['instances']
+  #   instances.each do |name, instance|
+  #     if instance['status'] == 'online'
+  #       member = Chef::Node.new
+  #       new_name = "#{name}.localdomain"
+  #       member.name(name)
+  #       member.default['fqdn'] = instance['private_dns_name']
+  #       member.default['ipaddress'] = instance['private_ip']
+  #       member.default['hostname'] = new_name
+  #       mongodb_attributes = {
+  #         # here we could support a map of instances to custom replicaset options in the custom json
+  #         'port' => node['mongodb']['config']['port'],
+  #         'replica_arbiter_only' => false,
+  #         # 'replica_build_indexes' => false,
+  #         'replica_hidden' => false,
+  #         'replica_slave_delay' => 0,
+  #         'replica_priority' => instance['private_ip'].gsub(/^.*\.(?=\d+)/, '').to_i,
+  #         'replica_tags' => {}, # to_hash is called on this
+  #         'replica_votes' => 1,
+  #         'config' => {
+  #           'port' => node['mongodb']['config']['port'],
+  #           'replica_arbiter_only' => false,
+  #           # 'replica_build_indexes' => true,
+  #           'replica_hidden' => false,
+  #           'replica_slave_delay' => 0,
+  #           'replica_priority' => instance['private_ip'].gsub(/^.*\.(?=\d+)/, '').to_i,
+  #           'replica_tags' => {}, # to_hash is called on this
+  #           'replica_votes' => 1
+  #         }
+  #       }
+  #       member.default['mongodb'] = mongodb_attributes
+  #       members << member
+  #     end
+  #   end
+  #   members
+  # end
 
   def self.replicaset_members(node)
     members = []
@@ -79,7 +79,7 @@ class Chef::ResourceDefinitionList::OpsWorksORMHelper
           member.default['fqdn'] = instance['private_dns_name']
           member.default['ipaddress'] = instance['private_ip']
           member.default['hostname'] = new_name
-          member.default['priority'] = instance['private_ip'].gsub(/^.*\.(?=\d+)/, '').to_i
+          member.default['priority'] = instance['private_ip'].gsub(/^.*\.(?=\d+)/, '').to_i/100
 
           # from mongodb overrides
           member.default['hidden'] = hidden_members.include?(name)
