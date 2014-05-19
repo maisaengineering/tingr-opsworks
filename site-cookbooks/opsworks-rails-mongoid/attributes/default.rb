29a07aaaae0a3ee4eee05f46e9b116f83b872499
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-18 18:38:59 -0700

    mognodb conf setup

--- site-cookbooks/opsworks-rails-mongoid/attributes/default.rb
+++ site-cookbooks/opsworks-rails-mongoid/attributes/default.rb
@@ -3,3 +3,26 @@ default['mongodb']['config']['bind_ip']="0.0.0.0"
 default['mongodb']['config']['port']="27017"
 default['mongodb']['config']['dbpath'] = "/data/mongodb"
 default['mongodb']['config']['logpath'] = "/data/log/mongodb/mongodb.log"
+
+
+# AWS STACK CUSTOm JSON
+# {
+#    "opsworks":{
+#       "data_bags":{
+#          "aws":{
+#             "main":{
+#                "aws_access_key_id":"",
+#                "aws_secret_access_key":""
+#             }
+#          }
+#       }
+#    },
+#    "mongodb":{
+#       "mms_agent":{
+#          "api_key":""
+#       },
+#       "layers" : "ds-mongo-rpl,dr-ds-mongo-rpl",
+#       "hidden" : "helios",
+#       "arbiter" : "hestia"
+#    }
+# }
---
+++ site-cookbooks/opsworks-rails-mongoid/recipes/mongo_mms_agent.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: opsworks-rails-mongoid
+# Recipe:: mongo_mms_agent
+#
+# Copyright (C) 2014 Chandan Benjaram
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "mongodb::mms-agent"
