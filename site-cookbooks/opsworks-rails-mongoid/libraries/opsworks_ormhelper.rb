8c890aaf6d6a307d5b0eca1e4dbc1972c3f1ef2a
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-18 23:03:09 -0700

    mognodb conf setup

--- site-cookbooks/opsworks-rails-mongoid/libraries/opsworks_ormhelper.rb
+++ site-cookbooks/opsworks-rails-mongoid/libraries/opsworks_ormhelper.rb
@@ -1,54 +1,6 @@
 require 'chef/node'
 
 class Chef::ResourceDefinitionList::OpsWorksORMHelper
-
-  # true if we're on opsworks, false otherwise
-  def self.opsworks?(node)
-    node['opsworks'] != nil
-  end
-
-  # return Chef Nodes for this replicaset / layer
-  # def self.replicaset_members_with_mongo_conf(node)
-  #   members = []
-  #   # FIXME -> this is bad, we're assuming replicaset instances use a single layer
-  #   node_layer = node['opsworks']['instance']['layers'].first
-  #   instances = node['opsworks']['layers'][replicaset_layer_slug_name]['instances']
-  #   instances.each do |name, instance|
-  #     if instance['status'] == 'online'
-  #       member = Chef::Node.new
-  #       new_name = "#{name}.localdomain"
-  #       member.name(name)
-  #       member.default['fqdn'] = instance['private_dns_name']
-  #       member.default['ipaddress'] = instance['private_ip']
-  #       member.default['hostname'] = new_name
-  #       mongodb_attributes = {
-  #         # here we could support a map of instances to custom replicaset options in the custom json
-  #         'port' => node['mongodb']['config']['port'],
-  #         'replica_arbiter_only' => false,
-  #         # 'replica_build_indexes' => false,
-  #         'replica_hidden' => false,
-  #         'replica_slave_delay' => 0,
-  #         'replica_priority' => instance['private_ip'].gsub(/^.*\.(?=\d+)/, '').to_i,
-  #         'replica_tags' => {}, # to_hash is called on this
-  #         'replica_votes' => 1,
-  #         'config' => {
-  #           'port' => node['mongodb']['config']['port'],
-  #           'replica_arbiter_only' => false,
-  #           # 'replica_build_indexes' => true,
-  #           'replica_hidden' => false,
-  #           'replica_slave_delay' => 0,
-  #           'replica_priority' => instance['private_ip'].gsub(/^.*\.(?=\d+)/, '').to_i,
-  #           'replica_tags' => {}, # to_hash is called on this
-  #           'replica_votes' => 1
-  #         }
-  #       }
-  #       member.default['mongodb'] = mongodb_attributes
-  #       members << member
-  #     end
-  #   end
-  #   members
-  # end
-
   def self.replicaset_members(node)
     members = []
     hidden_members = []
@@ -112,8 +64,8 @@ class Chef::ResourceDefinitionList::OpsWorksORMHelper
           @keyspace=v
         end
       end
-    rescue Mongo::OperationFailure => e
-      Chef::Log.warn("Mostly ReplicaSet not found. May be time to initialize? Exception=#{e}")
+    rescue => msg
+      Chef::Log.warn("Mostly ReplicaSet not found. May be time to initialize? Exception=#{msg}")
     end
 
     @keyspace
