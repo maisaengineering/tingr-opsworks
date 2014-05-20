5fa539ef8d8ed0a93dc04f5560b8e73fab082adf
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-20 08:05:02 -0700

    mongo conf cleanup

--- site-cookbooks/opsworks-rails-mongoid/recipes/configure.rb
+++ site-cookbooks/opsworks-rails-mongoid/recipes/configure.rb
@@ -10,7 +10,25 @@ node[:deploy].each do |application, deploy|
   aws = data_bag_item('aws', 'main')
   deploy = node[:deploy][application]
 
-  template "#{deploy[:deploy_to]}/current/config/mongoid.yml" do
+  template "#{deploy[:current_path]}/config/mongoid.yml" do
+    source "mongoid.yml.erb"
+    cookbook 'opsworks-rails-mongoid'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+
+    replicaset_instances = node["opsworks"]["layers"]["ds-mongo-rpl"]["instances"].keys.map{|server| "#{node["opsworks"]["layers"]["ds-mongo-rpl"]["instances"][server]["private_ip"]}:27017" }
+    variables(
+      :environment => deploy[:rails_env],
+      :replicaset_instances => replicaset_instances
+    )
+    # notifies :run, "execute[unicorn_restart]"
+    # only_if do
+    #   File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    # end
+  end
+
+  template "#{deploy[:shared_path]}/config/mongoid.yml" do
     source "mongoid.yml.erb"
     cookbook 'opsworks-rails-mongoid'
     mode "0660"
