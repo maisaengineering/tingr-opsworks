eada73e3ff7abab0f33a5abf6423d1300f75f263
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-22 03:38:41 -0700

    moving asset compilation step to deployment

--- site-cookbooks/opsworks-rails-mongoid/recipes/configure.rb
+++ site-cookbooks/opsworks-rails-mongoid/recipes/configure.rb
@@ -11,14 +11,13 @@ node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
   rails_env = new_resource.environment["RAILS_ENV"]
   replicaset_instances = node["opsworks"]["layers"]["ds-mongo-rpl"]["instances"].keys.map{|server| "#{node["opsworks"]["layers"]["ds-mongo-rpl"]["instances"][server]["private_ip"]}:27017" }
-
   Chef::Log.info("deploy... #{deploy}")
   Chef::Log.info("rails_env... #{rails_env}")
-  Chef::Log.info("release_path... #{release_path}")
+  Chef::Log.info("new_resource... #{new_resource}")
   Chef::Log.info("replicaset_instances... #{replicaset_instances}")
 
-  Chef::Log.info("configuring #{release_path}/config/mongoid.yml")
-  template "#{release_path}/config/mongoid.yml" do
+  Chef::Log.info("configuring #{deploy[:deploy_to]}/config/mongoid.yml")
+  template "#{deploy[:deploy_to]}/config/mongoid.yml" do
     source "mongoid.yml.erb"
     cookbook 'opsworks-rails-mongoid'
     mode "0660"
@@ -32,7 +31,7 @@ node[:deploy].each do |application, deploy|
 
   Chef::Log.info("precompiling assets for #{rails_env}...")
   execute "rake assets:precompile" do
-    cwd release_path
+    cwd deploy[:deploy_to]
     command "bundle exec rake assets:precompile"
     environment "RAILS_ENV" => rails_env
   end
