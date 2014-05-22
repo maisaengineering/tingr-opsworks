2b6284157b6e4cc2f5c90d33092c66b0e55704fd
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-22 06:21:02 -0700

    revert configure to regular after adding before_migrate + before_symlink assets to rails project

--- site-cookbooks/opsworks-rails-mongoid/recipes/configure.rb
+++ site-cookbooks/opsworks-rails-mongoid/recipes/configure.rb
@@ -6,67 +6,27 @@
 #
 # All rights reserved - Do Not Redistribute
 
-# node[:deploy].each do |application, deploy|
-#
-#   Chef::Log.info("rails_env... #{rails_env}")
-#
-#   # execute "unicorn_restart" do
-#   #   command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
-#   #   action :nothing
-#   # end
-#
-# end
-#
-# deploy_revision "/deploy/dir/" do
-#
-#   before_migrate do
-#     Chef::Log.info("inside before_migrate...before_migrate")
-#
-#   end
-# end
-
-before_migrate do
-  Chef::Log.info("CB executing before migrate inside")
-end
-
 
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
-  rails_env = deploy[:rails_env]
-  replicaset_instances = node["opsworks"]["layers"]["ds-mongo-rpl"]["instances"].keys.map{|server| "#{node["opsworks"]["layers"]["ds-mongo-rpl"]["instances"][server]["private_ip"]}:27017" }
-  Chef::Log.info("deploy... #{deploy}")
-  Chef::Log.info("deploy_to... #{deploy[:deploy_to]}")
 
-  Chef::Log.info("rails_env... #{rails_env}")
-  Chef::Log.info("replicaset_instances... #{replicaset_instances}")
-
-  Chef::Log.info("configuring #{deploy[:deploy_to]}/config/mongoid.yml")
-  # deploy[:before_migrate] do
-  #   Chef::Log.info("executing before migrate inside")
-  #   template "#{deploy[:deploy_to]}/shared/config/mongoid.yml" do
-  #     source "mongoid.yml.erb"
-  #     cookbook 'opsworks-rails-mongoid'
-  #     mode "0660"
-  #     group deploy[:group]
-  #     owner deploy[:user]
-  #     variables(
-  #       :environment => rails_env,
-  #       :replicaset_instances => replicaset_instances
-  #     )
-  #   end
-  # end
-
-
-  Chef::Log.info("precompiling assets for #{rails_env}...")
-  execute "rake assets:precompile" do
-    cwd "#{deploy[:deploy_to]}/current"
-    command "bundle exec rake assets:precompile"
-    environment "RAILS_ENV" => rails_env
+  template "#{deploy[:current_path]}/config/mongoid.yml" do
+    source "mongoid.yml.erb"
+    cookbook 'opsworks-rails-mongoid'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    replicaset_instances = node["opsworks"]["layers"]["ds-mongo-rpl"]["instances"].keys.map{|server| "#{node["opsworks"]["layers"]["ds-mongo-rpl"]["instances"][server]["private_ip"]}:27017" }
+    variables(
+      :environment => deploy[:rails_env],
+      :replicaset_instances => replicaset_instances
+    )
+    notifies :run, "execute[unicorn_restart]"
   end
 
-  # execute "unicorn_restart" do
-  #   command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
-  #   action :nothing
-  # end
+  execute "unicorn_restart" do
+    command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
+    action :nothing
+  end
 
 end
