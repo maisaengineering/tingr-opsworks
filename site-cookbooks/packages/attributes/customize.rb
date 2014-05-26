d0a9cea1963ee01b24ac438444e3468057e48b77
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-26 03:39:46 -0700

    berks stuff

--- site-cookbooks/packages/attributes/customize.rb
+++ site-cookbooks/packages/attributes/customize.rb
@@ -7,8 +7,13 @@
 # include_attribute "packages::customize"
 
 
+Chef::Log.info("CB echo 0...#{default[:packages]}")
 packages Mash.new unless attribute?("packages")
+
+Chef::Log.info("CB echo 1...#{default[:packages]}")
 
 # Toggle for recipes to determine if we should rely on distribution packages
 # or gems.
+Chef::Log.info("CB echo 2...#{default[:packages][:dist_only]}")
 packages[:dist_only] = false unless packages.has_key?(:dist_only)
+Chef::Log.info("CB echo 3...#{default[:packages][:dist_only]}")
