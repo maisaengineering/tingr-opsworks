6c09343503fee75efea3a54b7c9dedb8460991f6
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-26 03:37:25 -0700

    berks stuff

--- site-cookbooks/packages/attributes/customize.rb
+++ site-cookbooks/packages/attributes/customize.rb
@@ -1,7 +1,14 @@
 # MAISA override
-puts "CB echo 0...#{default[:packages]}"
-puts "CB echo 1...#{default[:packages][:dist_only]}"
-default[:packages][:dist_only] = false
+# puts "CB echo 0...#{default[:packages]}"
+# puts "CB echo 1...#{default[:packages][:dist_only]}"
+# default[:packages][:dist_only] = false
+#
+# puts "CB echo...include attributes"
+# include_attribute "packages::customize"
 
-puts "CB echo...include attributes"
-include_attribute "packages::customize"
+
+packages Mash.new unless attribute?("packages")
+
+# Toggle for recipes to determine if we should rely on distribution packages
+# or gems.
+packages[:dist_only] = false unless packages.has_key?(:dist_only)
