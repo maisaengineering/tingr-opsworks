f3bf589b6358bbc6e1859c295361089cfd20347e
Chandan Benjaram <chandan.benjaram@gmail.com>
2014-05-26 03:35:01 -0700

    berks stuff

--- site-cookbooks/packages/attributes/customize.rb
+++ site-cookbooks/packages/attributes/customize.rb
@@ -1,4 +1,7 @@
 # MAISA override
+puts "CB echo 0...#{default[:packages]}"
+puts "CB echo 1...#{default[:packages][:dist_only]}"
 default[:packages][:dist_only] = false
 
+puts "CB echo...include attributes"
 include_attribute "packages::customize"
